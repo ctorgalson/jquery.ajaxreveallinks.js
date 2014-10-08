9bbbaf7bf6cd6bbfd930b84891765186d4f5dc36
Christopher Torgalson <manager@bedlamhotel.com>
2014-10-08 21:41:49 +0200

    Wrap code in jquery block

--- scripts/jquery.ajaxreveallinks.js
+++ scripts/jquery.ajaxreveallinks.js
@@ -1,18 +1,21 @@
-/**
- * A tiny plugin to transform on or more links into Foundation's fancy ajax
- * reveal modals.
- */
-$.fn.ajaxRevealLinks = function (dataRevealId, source) {
-  return this.each(function () {
-    var $current = $(this),
-        $modal = $('#' + dataRevealId).attr({'data-reveal': ''});
-    if (window.location.pathname !== $current.attr('href')) {
-      $current
-        .attr({
-          'data-reveal-ajax': true,
-          'data-reveal-id': dataRevealId,
-          'href': source
-        });
-      }
-    });
-}; /* $.fn.ajaxRevealLinks */
+(function($){
+  "use strict";
+  /**
+   * A tiny plugin to transform on or more links into Foundation's fancy ajax
+   * reveal modals.
+   */
+  $.fn.ajaxRevealLinks = function (dataRevealId, source) {
+    return this.each(function () {
+      var $current = $(this),
+          $modal = $('#' + dataRevealId).attr({'data-reveal': ''});
+      if (window.location.pathname !== $current.attr('href')) {
+        $current
+          .attr({
+            'data-reveal-ajax': true,
+            'data-reveal-id': dataRevealId,
+            'href': source
+          });
+        }
+      });
+  }; /* $.fn.ajaxRevealLinks */
+})(jQuery);
